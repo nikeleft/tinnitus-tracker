182d87be8b239764d2a8a63efed53887f229d52a
nikeleft <nikeleft@gmail.com>
2026-03-06 20:53:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,20 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
-  // Δημιουργία του παραθύρου του browser.
   const win = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false, // Επιτρέπει στην εφαρμογή να χρησιμοποιεί λειτουργίες της Python/Node αν χρειαστεί
-    },
-    title: "SNR Adaptive App",
-    icon: path.join(__dirname, 'icon.ico') // Αν έχεις εικονίδιο
+      contextIsolation: false
+    }
   });
 
-  // Φόρτωση του index.html της εφαρμογής σου.
   win.loadFile('index.html');
-
-  // Προαιρετικό: Άνοιγμα των Developer Tools (για σφάλματα) - το κλείνεις πριν την πώληση
-  // win.webContents.openDevTools();
 }
 
-// Αυτή η μέθοδος καλείται όταν το Electron ολοκληρώσει την προετοιμασία
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady().then(createWindow);
 
-  app.on('activate', () => {
-    // Στα Mac είναι σύνηθες να ξαναδημιουργείται παράθυρο όταν πατάς το εικονίδιο
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-// Κλείσιμο της εφαρμογής όταν κλείσουν όλα τα παράθυρα (εκτός Mac)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
